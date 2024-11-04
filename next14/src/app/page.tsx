c6ca469d1a93d9e8b319c1326a2debe733e2a942
Akiagari-ua <nikita.my.email@gmail.com>
2024-11-04 17:56:53 +0200

    feat: add base apps

---
+++ next14/src/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+import PageWithText from '../../../src/pages/PageWithText'
+
+export default function Home() {
+  return (
+    <PageWithText title="Test page" text='In the midst of a shimmering autumn forest, a mysterious figure wandered quietly. Each step sent a gentle ripple through the fallen leaves, their rustling echoing softly through the trees. Above, the sunlight filtered through the branches, painting patterns of gold and green across the forest floor. The figure paused, as if listening to some hidden melody in the breeze. Somewhere in the distance, a bird called out, breaking the tranquil silence. And just like that, the figure was gone, disappearing into the depths of the woods, leaving only the whisper of leaves in their wake.' />
+  );
+}
